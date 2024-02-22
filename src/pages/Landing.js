0c866b70685b10e03ac949319a3fbc9226745f29
Moongram <qg8@rice.edu>
2024-02-22 16:47:34 -0600

    structure

---
+++ src/pages/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const Landing = () => {
+    const navigate = useNavigate();
+    return (
+        <div className='landing'>
+            <div className='form-container'>
+                <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt="logo" />
+                <button onClick={() => navigate('/login')}>Login</button>
+                <button onClick={() => navigate('/register')}>Register</button>
+            </div>
+        </div>
+    )
+
+}
+
+
+export default Landing;
