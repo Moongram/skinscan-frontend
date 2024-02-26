c9fa2173d6fe4f7b53a105215ac5ec51f22668d3
Ryan Ramos <ryan.ramosenhs@gmail.com>
2024-02-26 13:25:07 -0600

    removed google-react dependencies

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing  from './pages/Landing';
 
 function App(){
   return (
-    <GoogleOAuthProvider clientId="692214113342-atqchs2vkroi5macesfjmkr2p8ln7a7m.apps.googleusercontent.com">
-        <Router>
-            <Routes>
-                <Route path="/" element={<Landing />} />
+    <Router>
+        <Routes>
+            <Route path="/" element={<Landing />} />
 
-            </Routes>
-        </Router>
-    </GoogleOAuthProvider>
+        </Routes>
+    </Router>
   );
 }
 
