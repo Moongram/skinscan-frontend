adfcb0e395eced5378420b8dd29def514ea1baed
John Lynch <jlynch630@gmail.com>
2024-04-14 15:34:32 -0500

    some changes

--- src/components/ImageView.js
+++ src/components/ImageView.js
@@ -1,58 +1,72 @@
-import React, { useState, useRef, useEffect}  from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import { faArrowLeft, faArrowRight, faSyncAlt } from '@fortawesome/free-solid-svg-icons';
-import axios from 'axios';
+import {
+  faArrowLeft,
+  faArrowRight,
+  faSyncAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { LesionImage } from "./LesionImage";
 
-const ImageView = ({ selectedPatientId, leftImage, rightImage, toggleFilterVisibility, filterVisible}) => {
+const ImageView = ({
+  selectedPatientId,
+  leftImage,
+  rightImage,
+  toggleFilterVisibility,
+  filterVisible,
+}) => {
   const navigate = useNavigate();
-  const [name, setName] = useState('');
-  const [patientName, setPatientName] = useState('')
+  const [name, setName] = useState("");
+  const [patientName, setPatientName] = useState("");
   // Placeholder past images data
   const [images, setImages] = useState([]);
 
   useEffect(() => {
     async function getUserInfo() {
-      const response = await axios.get('http://localhost:4000/user', { withCredentials: true });
+      const response = await axios.get("http://localhost:4000/user", {
+        withCredentials: true,
+      });
 
-      if (response.statusText != 'OK') {
-        const message = `An error occurred: ${response.statusText}`;    
+      if (response.statusText != "OK") {
+        const message = `An error occurred: ${response.statusText}`;
         window.alert(message);
         return;
       }
 
       const retrievedUserInfo = response.data;
-    //   console.log(retrievedUseInfo)
-    setName(retrievedUserInfo.name)
+      //   console.log(retrievedUseInfo)
+      setName(retrievedUserInfo.name);
     }
 
-    getUserInfo()
-  }, [])
+    getUserInfo();
+  }, []);
 
   useEffect(() => {
     async function getImages() {
-        if (selectedPatientId) {
-            try {
-                const response = await axios.get(`http://localhost:4000/patient?id=${selectedPatientId}`, 
-                    {withCredentials: true}
-                );
-                console.log(response.data);
-                setPatientName(response.data.name)
-                setImages(response.data.images)
-            } catch (error) {
-                console.error('Error getting image:', error);
-                alert('Error getting image');
-            }
+      if (selectedPatientId) {
+        try {
+          const response = await axios.get(
+            `http://localhost:4000/patient?id=${selectedPatientId}`,
+            { withCredentials: true }
+          );
+          console.log(response.data);
+          setPatientName(response.data.name);
+          setImages(response.data.images);
+        } catch (error) {
+          console.error("Error getting image:", error);
+          alert("Error getting image");
         }
+      }
     }
 
     setSelectedLeftImage(null);
     setSelectedRightImage(null);
-    getImages()
-  }, [selectedPatientId])
+    getImages();
+  }, [selectedPatientId]);
 
   const transformComponentLeft = useRef();
   const transformComponentRight = useRef();
@@ -61,130 +75,139 @@ const ImageView = ({ selectedPatientId, leftImage, rightImage, toggleFilterVisib
   const [zoomLevelLeft, setZoomLevelLeft] = useState(1);
   const [zoomLevelRight, setZoomLevelRight] = useState(1);
   const [syncImages, setSyncImages] = useState(false);
-    // Flags to prevent infinite mutual updates
-    const updatingLeft = useRef(false);
-    const updatingRight = useRef(false);
+  // Flags to prevent infinite mutual updates
+  const updatingLeft = useRef(false);
+  const updatingRight = useRef(false);
 
-    const handleTransformLeft = (e) => {
-      if (syncImages && !updatingRight.current) {
-          updatingLeft.current = true;
-          transformComponentRight.current.setTransform(
-              e.instance.transformState.positionX,
-              e.instance.transformState.positionY,
-              e.instance.transformState.scale,
-              0
-          );
-          updatingLeft.current = false;
-      }
+  const handleTransformLeft = (e) => {
+    if (syncImages && !updatingRight.current) {
+      updatingLeft.current = true;
+      transformComponentRight.current.setTransform(
+        e.instance.transformState.positionX,
+        e.instance.transformState.positionY,
+        e.instance.transformState.scale,
+        0
+      );
+      updatingLeft.current = false;
+    }
   };
 
   const handleTransformRight = (e) => {
-      if (syncImages && !updatingLeft.current) {
-          updatingRight.current = true;
-          transformComponentLeft.current.setTransform(
-              e.instance.transformState.positionX,
-              e.instance.transformState.positionY,
-              e.instance.transformState.scale,
-              0
-          );
-          updatingRight.current = false;
-      }
+    if (syncImages && !updatingLeft.current) {
+      updatingRight.current = true;
+      transformComponentLeft.current.setTransform(
+        e.instance.transformState.positionX,
+        e.instance.transformState.positionY,
+        e.instance.transformState.scale,
+        0
+      );
+      updatingRight.current = false;
+    }
   };
   const toggleSyncImages = () => {
     setSyncImages(!syncImages);
-};
-const [matchResult, setMatchResult] = useState(null);  
-const [leftLesionCoordinates, setLeftLesionCoordinates] = useState(null);
-const [rightLesionCoordinates, setRightLesionCoordinates] = useState(null);
-    const checkMatch = async () => {
-        if (!selectedLeftImage || !selectedRightImage) {
-            alert('Both images must be selected to check for a match.');
-            return;
-        }
+  };
+  const [matchResult, setMatchResult] = useState(null);
+  const [leftLesionCoordinates, setLeftLesionCoordinates] = useState(null);
+  const [rightLesionCoordinates, setRightLesionCoordinates] = useState(null);
+  const checkMatch = async () => {
+    if (!selectedLeftImage || !selectedRightImage) {
+      alert("Both images must be selected to check for a match.");
+      return;
+    }
 
-        try {
-            const response = await axios.get(`http://localhost:4000/match?a=${selectedLeftImage.id}&b=${selectedRightImage.id}`, { withCredentials: true });
-            setMatchResult(response.data);
-            console.log(matchResult, selectedLeftImage.id, selectedRightImage.id);
-        } catch (error) {
-            console.error('Failed to fetch match data:', error);
-            alert('Failed to check images match');
-        }
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/match?a=${selectedLeftImage.id}&b=${selectedRightImage.id}`,
+        { withCredentials: true }
+      );
+      setMatchResult(response.data);
+      console.log(matchResult, selectedLeftImage.id, selectedRightImage.id);
+    } catch (error) {
+      console.error("Failed to fetch match data:", error);
+      alert("Failed to check images match");
+    }
 
-        try {
-            const response = await axios.get(`http://localhost:4000/lesion?id=${selectedLeftImage.id}`, { withCredentials: true });
-            setLeftLesionCoordinates(response.data);
-            console.log(leftLesionCoordinates);
-        } catch (error) {
-            console.error('Failed to fetch lesion data:', error);
-            alert('Failed to check images match');
-        }
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/lesion?id=${selectedLeftImage.id}`,
+        { withCredentials: true }
+      );
+      setLeftLesionCoordinates(response.data);
+      console.log(leftLesionCoordinates);
+    } catch (error) {
+      console.error("Failed to fetch lesion data:", error);
+      alert("Failed to check images match");
+    }
 
-        try {
-            const response = await axios.get(`http://localhost:4000/lesion?id=${selectedRightImage.id}`, { withCredentials: true });
-            setRightLesionCoordinates(response.data);
-            console.log(rightLesionCoordinates);
-        }
-        catch (error) {
-            console.error('Failed to fetch lesion data:', error);
-            alert('Failed to check images match');
-        }
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/lesion?id=${selectedRightImage.id}`,
+        { withCredentials: true }
+      );
+      setRightLesionCoordinates(response.data);
+      console.log(rightLesionCoordinates);
+    } catch (error) {
+      console.error("Failed to fetch lesion data:", error);
+      alert("Failed to check images match");
+    }
+  };
 
-    };
+  // const handleTransformLeft = (e) => {
+  //     if (!updatingRight.current) {
+  //         updatingLeft.current = true;
+  //         transformComponentRight.current.setTransform(
+  //             e.instance.transformState.positionX,
+  //             e.instance.transformState.positionY,
+  //             e.instance.transformState.scale,
+  //             0
+  //         );
+  //         updatingLeft.current = false;
+  //     }
+  // };
 
-    // const handleTransformLeft = (e) => {
-    //     if (!updatingRight.current) {
-    //         updatingLeft.current = true;
-    //         transformComponentRight.current.setTransform(
-    //             e.instance.transformState.positionX,
-    //             e.instance.transformState.positionY,
-    //             e.instance.transformState.scale,
-    //             0
-    //         );
-    //         updatingLeft.current = false;
-    //     }
-    // };
-
-    // const handleTransformRight = (e) => {
-    //     if (!updatingLeft.current) {
-    //         updatingRight.current = true;
-    //         transformComponentLeft.current.setTransform(
-    //             e.instance.transformState.positionX,
-    //             e.instance.transformState.positionY,
-    //             e.instance.transformState.scale,
-    //             0
-    //         );
-    //         updatingRight.current = false;
-    //     }
-    // };
+  // const handleTransformRight = (e) => {
+  //     if (!updatingLeft.current) {
+  //         updatingRight.current = true;
+  //         transformComponentLeft.current.setTransform(
+  //             e.instance.transformState.positionX,
+  //             e.instance.transformState.positionY,
+  //             e.instance.transformState.scale,
+  //             0
+  //         );
+  //         updatingRight.current = false;
+  //     }
+  // };
   leftImage = {
-    url: '/images/lesion1.jpg',
-    name: 'Jane Doe',
-    photoId: '13',
-    timestamp: '2024-03-20 10:00',
+    url: "/images/lesion1.jpg",
+    name: "Jane Doe",
+    photoId: "13",
+    timestamp: "2024-03-20 10:00",
   };
 
   rightImage = {
-    url: '/images/lesion2.jpg',
-    name: 'John Smith',
-    photoId: '14',
-    timestamp: '2024-03-21 11:00',
+    url: "/images/lesion2.jpg",
+    name: "John Smith",
+    photoId: "14",
+    timestamp: "2024-03-21 11:00",
   };
-  
+
   const convertTimeZone = (time) => {
     const date = new Date(time);
-    const cdtTime = date.toLocaleString('en-US', { timeZone: 'America/Chicago' });
-    return cdtTime
-  }
-  
+    const cdtTime = date.toLocaleString("en-US", {
+      timeZone: "America/Chicago",
+    });
+    return cdtTime;
+  };
+
   const [selectedLeftImage, setSelectedLeftImage] = useState(null);
   const [selectedRightImage, setSelectedRightImage] = useState(null);
 
-  const [lastClickedPosition, setLastClickedPosition] = useState('right');
+  const [lastClickedPosition, setLastClickedPosition] = useState("right");
 
   const handleImageClick = (image) => {
-    const newPosition = lastClickedPosition === 'left' ? 'right' : 'left';
-    if (newPosition === 'left') {
+    const newPosition = lastClickedPosition === "left" ? "right" : "left";
+    if (newPosition === "left") {
       setSelectedLeftImage(image);
     } else {
       setSelectedRightImage(image);
@@ -200,7 +223,7 @@ const [rightLesionCoordinates, setRightLesionCoordinates] = useState(null);
   };
 
   const navigateToUpload = () => {
-    navigate('/uploading');
+    navigate("/uploading");
   };
 
   const scrollToEnd = () => {
@@ -210,66 +233,103 @@ const [rightLesionCoordinates, setRightLesionCoordinates] = useState(null);
   };
   return (
     <div className="image-view-wrapper">
-    
       <div className="zoom-controls">
-      <button className="toggle-filter-button" onClick={toggleFilterVisibility}>
-                <FontAwesomeIcon icon={filterVisible ? faArrowLeft : faArrowRight} />
+        <button
+          className="toggle-filter-button"
+          onClick={toggleFilterVisibility}
+        >
+          <FontAwesomeIcon icon={filterVisible ? faArrowLeft : faArrowRight} />
         </button>
         <button onClick={toggleSyncImages}>
-          <FontAwesomeIcon icon={faSyncAlt} /> {syncImages ? 'Unsync' : 'Sync'} Images
+          <FontAwesomeIcon icon={faSyncAlt} /> {syncImages ? "Unsync" : "Sync"}{" "}
+          Images
         </button>
         <button onClick={checkMatch} className="match-check-button">
-                Check If Images Match
-            </button>
-        <div className="user-info">
-          Logged in as {name}
-        </div>
-        <button className="upload-button" onClick={() => navigateToUpload(
-          '/uploading'
-        )}>Upload Image</button>
+          Check If Images Match
+        </button>
+        <div className="user-info">Logged in as {name}</div>
+        <button
+          className="upload-button"
+          onClick={() => navigateToUpload("/uploading")}
+        >
+          Upload Image
+        </button>
       </div>
-    <div className="image-container-wrapper">
-    <div className="image-container" style={{ transform: `scale(${zoomLevelLeft})` }}>
-        {selectedLeftImage !== null && <div className="image-info">
-          <h3>{patientName}</h3>
-    
-          <h3>ID: {selectedLeftImage.id}</h3>
-          <h3>Time: {convertTimeZone(selectedLeftImage.timestamp)}</h3>
-        </div>}
-    
-        <TransformWrapper
-        ref={transformComponentLeft}
-        onTransformed={handleTransformLeft}>
-            <TransformComponent>
-            {selectedLeftImage !== null && <img src={selectedLeftImage.url} alt="test" />}
-            {selectedLeftImage === null && <h4 className="select-image-text"> SELECT AN IMAGE BELOW </h4>}
-            </TransformComponent>
-        </TransformWrapper>
-      </div>
-      <div className="divider"></div>
-      <div className="image-container" style={{ transform: `scale(${zoomLevelRight})` }}>
-        {selectedRightImage !== null && <div className="image-info">
-          <h3>{patientName}</h3>
-          <h3>ID: {selectedRightImage.id}</h3>
-          <h3>Time: {convertTimeZone(selectedRightImage.timestamp)}</h3>
-        </div>}
-      
-        <TransformWrapper
-        ref={transformComponentRight}
-        onTransformed={handleTransformRight}>
-            <TransformComponent>
-                {selectedRightImage !== null && <img src={selectedRightImage.url} alt="test" />}
-                {selectedRightImage === null && <h4 className="select-image-text"> SELECT AN IMAGE BELOW </h4>}
-            </TransformComponent>
-        </TransformWrapper>
-      </div>
-    </div>
-    <div className="image-note">
-      Click two images to replace left and right images, scroll to view more past images
-    </div>
+      <div className="image-container-wrapper">
+        <div
+          className="image-container"
+          style={{ transform: `scale(${zoomLevelLeft})` }}
+        >
+          {selectedLeftImage !== null && (
+            <div className="image-info">
+              <h3>{patientName}</h3>
 
-    <div className="image-bar-container">
-    <button className="scroll-button left" onClick={scrollToStart}>
+              <h3>ID: {selectedLeftImage.id}</h3>
+              <h3>Time: {convertTimeZone(selectedLeftImage.timestamp)}</h3>
+            </div>
+          )}
+
+          <TransformWrapper
+            ref={transformComponentLeft}
+            onTransformed={handleTransformLeft}
+          >
+            <TransformComponent>
+              {selectedLeftImage !== null && (
+                <LesionImage
+                  imgSrc={selectedLeftImage.url}
+                  isA={true}
+                  lesions={leftLesionCoordinates?.lesions || []}
+                  matchRes={matchResult?.mappings}
+                  highlight={true}
+                />
+              )}
+              {selectedLeftImage === null && (
+                <h4 className="select-image-text"> SELECT AN IMAGE BELOW </h4>
+              )}
+            </TransformComponent>
+          </TransformWrapper>
+        </div>
+        <div className="divider"></div>
+        <div
+          className="image-container"
+          style={{ transform: `scale(${zoomLevelRight})` }}
+        >
+          {selectedRightImage !== null && (
+            <div className="image-info">
+              <h3>{patientName}</h3>
+              <h3>ID: {selectedRightImage.id}</h3>
+              <h3>Time: {convertTimeZone(selectedRightImage.timestamp)}</h3>
+            </div>
+          )}
+
+          <TransformWrapper
+            ref={transformComponentRight}
+            onTransformed={handleTransformRight}
+          >
+            <TransformComponent>
+              {selectedRightImage !== null && (
+                <LesionImage
+                  imgSrc={selectedRightImage.url}
+                  isA={false}
+                  matchRes={matchResult?.mappings}
+                  lesions={leftLesionCoordinates?.lesions || []}
+                  highlight={true}
+                />
+              )}
+              {selectedRightImage === null && (
+                <h4 className="select-image-text"> SELECT AN IMAGE BELOW </h4>
+              )}
+            </TransformComponent>
+          </TransformWrapper>
+        </div>
+      </div>
+      <div className="image-note">
+        Click two images to replace left and right images, scroll to view more
+        past images
+      </div>
+
+      <div className="image-bar-container">
+        <button className="scroll-button left" onClick={scrollToStart}>
           <FontAwesomeIcon icon={faArrowLeft} />
         </button>
         <div className="image-bar" ref={imageBarRef}>
@@ -287,8 +347,7 @@ const [rightLesionCoordinates, setRightLesionCoordinates] = useState(null);
           <FontAwesomeIcon icon={faArrowRight} />
         </button>
       </div>
-  </div>
-  
+    </div>
   );
 };
 export default ImageView;
---
+++ src/components/LesionImage.js
@@ -0,0 +1,84 @@
+import { useEffect, useRef, useState } from "react";
+
+/**
+ *
+ * @param {{
+ * lesions: {
+ *  x: number,
+ *  y: number,
+ *  radius: number,
+ *  id: number,
+ *  bodyPart: number
+ * }[];
+ * imgSrc: string;
+ * matchRes?: { a: number, b: number }[];
+ * isA: boolean;
+ * highlight?: boolean;
+ * }} props
+ */
+export const LesionImage = ({ lesions, imgSrc, isA, matchRes, highlight }) => {
+  /**
+   * @type {import("react").MutableRefObject<HTMLCanvasElement | null>}
+   */
+  const canvasRef = useRef(null);
+
+  /**
+   * @type {[CanvasRenderingContext2D | null, any]}
+   */
+  const [ctx, setCtx] = useState(null);
+
+  const [img, setImage] = useState(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    setCtx(ctx);
+  }, []);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const img = new Image();
+    img.src = imgSrc;
+    canvasRef.current.width = img.width;
+    canvasRef.current.height = img.height;
+    img.onload = () => {
+      setImage(img);
+    };
+  }, [imgSrc]);
+
+  useEffect(() => {
+    if (!ctx || !img) return;
+
+    /**
+     * @type {CanvasRenderingContext2D}
+     */
+    const ctx2 = ctx;
+    ctx.drawImage(img, 0, 0);
+    ctx2.strokeStyle = "rgba(255,0,0,0.4)";
+    ctx2.lineWidth = 2 * devicePixelRatio;
+    if (highlight) {
+      lesions.forEach((l) => {
+        const id = matchRes?.findIndex((r) => (isA ? r.a : r.b) === l.id) || 0;
+
+        ctx2.strokeStyle = getColorForId(id);
+        ctx.beginPath();
+        ctx.arc(l.x, l.y, l.radius, 0, 359.9999);
+        ctx2.stroke();
+      });
+    }
+  }, [img, lesions, highlight, ctx, isA, matchRes]);
+  return <canvas className="lesion-canvas" ref={canvasRef}></canvas>;
+};
+
+const getColorForId = (id) => {
+  //h(x)=ax^2+bx+c mod R
+  const a = 924755240;
+  const b = 374679788;
+  const c = 568558853;
+  const p = 1000001699;
+  const h = a * (id * id) + b * id + c;
+  return colors[(h % p) % colors.length];
+};
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "purple"];
